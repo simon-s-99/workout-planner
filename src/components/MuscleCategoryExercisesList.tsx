8b835b116d853a5c7b2d4c0ee1166b2f5a5294e7
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-03-02 11:10:43 +0100

    api call & result display done

--- src/components/MuscleCategoryExercisesList.tsx
+++ src/components/MuscleCategoryExercisesList.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { ExerciseObject } from "../types";
 
 interface MuscleCategoryProps {
     muscleGroup: string;
@@ -6,12 +7,43 @@ interface MuscleCategoryProps {
 }
 
 const MuscleCategoryExercisesList: React.FC<MuscleCategoryProps> = ({ muscleGroup, renderList }) => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [exercises, setExercises] = useState<ExerciseObject[]>([]);
+
+    useEffect(() => {
+        // this is a base request, we can build on this later to add exercise type (cardio/strongman/plyometrics/etc.)
+        // it is also possible to allow the user to flip through pages of a selected exercise type
+        // there is also the possibility to allow the user to search for specific types of exercises
+        // ex.: "press" search would give us all exercises with the word "press" in the name 
+        const url: URL = new URL("https://api.api-ninjas.com/v1/exercises");
+        url.searchParams.set("muscle", muscleGroup);
+
+        fetch(url, {
+            headers: {
+                "X-Api-Key": "hRwPXK6YvKgUbEKzGW01sw==YttRwZHUK8grKBfI"
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => setExercises(data))
+            .then(() => setLoading(false))
+            .catch((error) => {
+                console.log(error.message);
+            });
+
+    }, [muscleGroup]);
+
     if (renderList) {
-        const [exercises, setExercises] = useState([]);
-        
+        // if the api call is ever slow enough to display a "loading" phase
+        // then "..." is displayed to the user so that they have some indication that the page is loading
         return (
             <ul className="muscleGroupCategoryExercisesList">
-                <li>{muscleGroup}</li> {/* <- placeholder, put api results here */}
+                {loading ? (
+                    <li>...</li>
+                ) : (
+                    exercises.map((exercise, index) => (
+                        <li key={index}>{exercise.name}</li>
+                    ))
+                )}
             </ul>
         );
     }

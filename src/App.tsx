68f18b3ea96d26212e247769c36153a50dd45ecb
Sam <zaaamuel_96@hotmail.com>
2024-03-04 18:34:56 +0100

    Added comments, and moved code into a new hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import SetRepsWeight from "./components/SetRepsWeight";
-import type {
-  Settings,
-  Weekday,
-  ExerciseObject,
-  WeekdayExerciseMap,
-} from "./types";
+import type { Settings, Weekday, ExerciseObject } from "./types";
 import MuscleCategoryList from "./components/MuscleCategoryList";
 import WeekdayPicker from "./components/WeekdayPicker";
 import UnitsPicker from "./components/UnitsPicker";
-
-// Utility to serialize a WeekdayExerciseMap to a string
-const serializeWeekdayExerciseMap = (map: WeekdayExerciseMap): string => {
-  const entries = Array.from(map.entries());
-  return JSON.stringify(entries);
-};
-
-// Utility to deserialize a string back to a WeekdayExerciseMap
-const deserializeWeekdayExerciseMap = (
-  serializedMap: string
-): WeekdayExerciseMap => {
-  const entries: [Weekday, ExerciseObject[]][] = JSON.parse(serializedMap);
-  return new Map(entries);
-};
-
-// Reads exercises for a specific weekday from localStorage
-const readLocalStorageForWeekday = (weekday: Weekday): ExerciseObject[] => {
-  const serializedMap = localStorage.getItem("exercises");
-  if (serializedMap) {
-    const exercisesMap = deserializeWeekdayExerciseMap(serializedMap);
-    return exercisesMap.get(weekday) || [];
-  }
-  return [];
-};
-
-// Writes exercises for a specific weekday to localStorage
-const writeLocalStorageForWeekday = (
-  weekday: Weekday,
-  exercises: ExerciseObject[]
-) => {
-  const serializedMap = localStorage.getItem("exercises");
-  const exercisesMap = serializedMap
-    ? deserializeWeekdayExerciseMap(serializedMap)
-    : new Map();
-  exercisesMap.set(weekday, exercises);
-  localStorage.setItem("exercises", serializeWeekdayExerciseMap(exercisesMap));
-};
+import { useExerciseStorage } from "./hooks/useExerciseStorage";
 
 const App: React.FC = () => {
   const [settings, setSettings] = useState<Settings>({
@@ -55,20 +14,30 @@ const App: React.FC = () => {
   });
   const [selectedWeekday, setSelectedWeekday] = useState<Weekday>("Monday");
   const [exercises, setExercises] = useState<ExerciseObject[]>([]);
+  const { read, write } = useExerciseStorage();
 
+  // This useEffect hook is used to fetch and set the exercises for the selected weekday
+  // whenever the selectedWeekday changes. It leverages the `read` function from the
+  // useExerciseStorage hook to fetch the exercises from localStorage.
   useEffect(() => {
-    const storedExercises = readLocalStorageForWeekday(selectedWeekday);
-    setExercises(storedExercises);
-  }, [selectedWeekday]);
+    setExercises(read(selectedWeekday));
+  }, [selectedWeekday, read]);
 
+  // This function handles updates to an individual exercise. It's typically triggered
+  // by some user action in the UI, such as completing an exercise or updating its details.
   const handleExerciseUpdate = (updatedExercise: ExerciseObject) => {
+    // Maps over the current list of exercises, looking for the exercise that matches it
+
     const updatedExercises = exercises.map((exercise) =>
       exercise.name === updatedExercise.name
         ? { ...exercise, ...updatedExercise }
         : exercise
     );
+
     setExercises(updatedExercises);
-    writeLocalStorageForWeekday(selectedWeekday, updatedExercises);
+
+    // Calls the "write" function with the currently selected weekday and the updated list
+    write(selectedWeekday, updatedExercises);
   };
 
   return (

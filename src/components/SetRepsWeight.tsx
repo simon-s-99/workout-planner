f581f548c0be591df0ff6d85674320150ccf0c9b
Sam <zaaamuel_96@hotmail.com>
2024-03-05 22:05:44 +0100

    fml

--- src/components/SetRepsWeight.tsx
+++ src/components/SetRepsWeight.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
-import type { ExerciseObject, Weekday, WorkingSet } from "../types";
+import type {
+  ExerciseObject,
+  Weekday,
+  WeekdayExerciseMap,
+  WorkingSet,
+} from "../types";
 import { useLocalStorageRead } from "../hooks/useLocalStorageRead";
+import { useLocalStorageWrite } from "../hooks/useLocalStorageWrite";
 
 type SetRepsWeightProps = {
   // exercise: ExerciseObject;
@@ -14,6 +20,71 @@ const SetRepsWeight: React.FC<SetRepsWeightProps> = ({ weekday }) => {
 
   //form, when submitted send to localstorage. Give write an exercisemap,
   // pair weekdayexercies with the weekday and send to localstorageWrite
+
+  const [exercises, setExercises] = useState(weekdayExercises);
+
+  // Function to add a set to an exercise
+  const addSet = (
+    exerciseIndex: number,
+    weekday: Weekday,
+    exercises: ExerciseObject[]
+  ): void => {
+    const newExercises = [...exercises];
+    const newSet = { repetitions: 0, weight: 0, completed: false };
+    newExercises[exerciseIndex].sets.push(newSet);
+
+    // Use React's useState hook or a similar state management solution to update exercises
+    // setExercises(newExercises);
+
+    // Construct a new WeekdayExerciseMap
+    const weekdayExerciseMap: WeekdayExerciseMap = new Map();
+    weekdayExerciseMap.set(weekday, newExercises);
+
+    // Using the useLocalStorageWrite function with the newly constructed Map
+    useLocalStorageWrite(weekdayExerciseMap);
+  };
+
+  // Function to remove a set from an exercise
+  const removeSet = (exerciseIndex: number, setIndex: number): void => {
+    const newExercises = [...exercises];
+    newExercises[exerciseIndex].sets.splice(setIndex, 1);
+    setExercises(newExercises);
+    // Update local storage accordingly
+  };
+
+  // Function to toggle a set as completed
+  const toggleSetCompleted = (
+    exerciseIndex: number,
+    setIndex: number
+  ): void => {
+    const newExercises = [...exercises];
+    const set = newExercises[exerciseIndex].sets[setIndex];
+    set.completed = !set.completed;
+    setExercises(newExercises);
+    // Update local storage accordingly
+  };
+
+  // Function to toggle all sets as completed for an exercise
+  const toggleAllSetsCompleted = (exerciseIndex: number): void => {
+    const newExercises = [...exercises];
+    const sets = newExercises[exerciseIndex].sets;
+    const areAllCompleted = sets.every((set) => set.completed);
+    newExercises[exerciseIndex].sets = sets.map((set) => ({
+      ...set,
+      completed: !areAllCompleted,
+    }));
+    setExercises(newExercises);
+    // Update local storage accordingly
+  };
+
+  // Function to toggle an exercise as completed (assuming you have a completed flag in ExerciseObject)
+  const toggleExerciseCompleted = (exerciseIndex: number): void => {
+    const newExercises = [...exercises];
+    newExercises[exerciseIndex].completed =
+      !newExercises[exerciseIndex].completed;
+    setExercises(newExercises);
+    // Update local storage accordingly
+  };
 
   return (
     <ol>
@@ -29,179 +100,23 @@ const SetRepsWeight: React.FC<SetRepsWeightProps> = ({ weekday }) => {
                 </p>
               </li>
             ))}
+            {/* Add Set button for each exercise */}
+            <li>
+              <button onClick={() => addSet(index, weekday, weekdayExercises)}>
+                Add Set
+              </button>
+            </li>
+            <li>
+              <button onClick={() => removeSet(exerciseIndex, setIndex)}>
+                ❌
+              </button>
+            </li>
           </ol>
         </li>
       ))}
     </ol>
   );
 };
-
-// State for the editable sets of the exercise
-// const [editableSets, setEditableSets] = useState<WorkingSet[]>(weekdayExercises.sets);
-// // Toggle visibility of sets
-// const [showSets, setShowSets] = useState(false);
-// // Track if the exercise is completed
-// const [isExerciseCompleted, setIsExerciseCompleted] = useState(
-//   exercise.completed
-// );
-
-// Effect hook to update state when the exercise prop changes
-// useEffect(() => {
-//   setEditableSets(exercise.sets);
-//   setIsExerciseCompleted(exercise.sets.every((set) => set.completed));
-// }, [exercise]);
-
-// const toggleShowSets = () => setShowSets(!showSets);
-
-// const handleSetChange = (
-//   index: number,
-//   field: "weight" | "repetitions",
-//   value: number
-// ) => {
-//   // const updatedSets = editableSets.map((set, idx) =>
-//   //   idx === index ? { ...set, [field]: value } : set
-//   // );
-//   // setEditableSets(updatedSets);
-//   // updateExercise({ ...exercise, sets: updatedSets });
-// };
-
-const addSet = () => {
-  const newSet: WorkingSet = {
-    repetitions: 10,
-    weight: 100,
-    completed: false,
-  };
-  // const updatedSets = [...editableSets, newSet];
-  // setEditableSets(updatedSets);
-  // updateExercise({ ...exercise, sets: updatedSets });
-};
-
-const removeSet = (index: number) => {
-  // const updatedSets = editableSets.filter((_, idx) => idx !== index);
-  // setEditableSets(updatedSets);
-  // updateExercise({ ...exercise, sets: updatedSets });
-};
-
-// const toggleSetCompleted = (index: number) => {
-//   const updatedSets = editableSets.map((set, idx) =>
-//     idx === index ? { ...set, completed: !set.completed } : set
-//   );
-//   // setEditableSets(updatedSets);
-//   // updateExercise({ ...exercise, sets: updatedSets });
-// };
-
-// const toggleAllSetsCompleted = () => {
-//   // Check if all sets are already marked as completed
-//   const allCompleted = editableSets.every((set) => set.completed);
-
-//   const updatedSets = editableSets.map((set) => ({
-//     ...set,
-//     completed: !allCompleted,
-//   }));
-
-// setEditableSets(updatedSets);
-// updateExercise({
-//   ...exercise,
-//   sets: updatedSets,
-//   completed: updatedSets.every((set) => set.completed),
-// });
-// };
-
-// Function to handle change in the exercise's completion status
-// const handleExerciseCompletedChange = () => {
-//   const newCompletionStatus = !isExerciseCompleted;
-//   setIsExerciseCompleted(newCompletionStatus);
-//   if (newCompletionStatus) {
-//     setShowSets(false); // Close the sets if the exercise is marked as completed
-//   }
-//   const updatedSets = editableSets.map((set) => ({
-//     ...set,
-//     completed: newCompletionStatus,
-//   }));
-//   setEditableSets(updatedSets);
-//   updateExercise({
-//     ...exercise,
-//     sets: updatedSets,
-//     completed: newCompletionStatus,
-//   });
-// };
-
-//     ))}
-
-// {
-//   /* Interactive block for the current exercise */
-// }
-//   <div className="exercise-block">
-//     {/* <h3 onClick={toggleShowSets}>{exercise.name}</h3> */}
-//     <div className="input-and-button-container">
-//       {/* Dynamic interaction UI for editing sets */}
-//       <span className="total-sets-info">Total Sets: {editableSets.length}</span>
-//       <div className="flex-container">
-//         <button
-//           onClick={handleExerciseCompletedChange}
-//           className="exercise-completed-button"
-//         >
-//           Exercise Completed
-//         </button>
-//         <label className="flex-label">
-//           <input
-//             type="checkbox"
-//             checked={editableSets.every((set) => set.completed)}
-//             onChange={toggleAllSetsCompleted}
-//             className="checkbox-margin"
-//           />
-//           All sets completed
-//         </label>
-//         <button onClick={addSet} className="add-button">
-//           Add Set
-//         </button>
-//       </div>
-//     </div>
-//   </div>;
-
-//   {
-//     showSets &&
-//       editableSets.map((set, index) => (
-//         <div key={index} className="exercise-block exercise-block-background">
-//           <span>{`Set ${index + 1}: `}</span>
-//           <label className="label-margin">
-//             Reps:
-//             <input
-//               type="number"
-//               value={set.repetitions}
-//               onChange={(e) =>
-//                 handleSetChange(index, "repetitions", parseInt(e.target.value))
-//               }
-//               className="input-style"
-//             />
-//           </label>
-//           <label className="label-margin">
-//             Weight:
-//             <input
-//               type="number"
-//               value={set.weight}
-//               onChange={(e) =>
-//                 handleSetChange(index, "weight", parseInt(e.target.value))
-//               }
-//               className="input-style"
-//             />
-//           </label>
-//           <label className="checkbox-label">
-//             <input
-//               type="checkbox"
-//               checked={set.completed}
-//               onChange={() => toggleSetCompleted(index)}
-//               className="checkbox-margin"
-//             />
-//             Completed
-//           </label>
-//           <button onClick={() => removeSet(index)} className="remove-button">
-//             Remove Set
-//           </button>
-//         </div>
-//       ));
-//   }
-//  };
 
 const styles = {
   flexContainer: {

5a9a9b53ee984add302764aa5f4001dce4c4f525
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-03-08 10:44:11 +0100

    MuscleAnatomyChart | Testing viable anatomy .svg

--- src/components/MuscleAnatomy/MuscleAnatomyChart.tsx
+++ src/components/MuscleAnatomy/MuscleAnatomyChart.tsx
@@ -1,11 +1,16 @@
-import { Weekday } from "../types";
+import { Weekday } from "../../types";
+import FullBody from "./FullBody";
 
 interface MuscleAnatomyChartProps {
     weekday: Weekday;
 }
 
 const MuscleAnatomyChart: React.FC<MuscleAnatomyChartProps> = ({ weekday }) => {
-    return null; // <- temp
+    return (
+        <div>
+            <FullBody fill={false} />
+        </div>
+    )
 }
 
 export default MuscleAnatomyChart;

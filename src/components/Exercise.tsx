785d9c03144b9aa41ab0324796348cab034169de
Sam <zaaamuel_96@hotmail.com>
2024-03-09 13:51:11 +0100

    fixed some CSS, and toggleExercise function

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -19,7 +19,7 @@ import { useLocalStorageWrite } from "../hooks/useLocalStorageWrite";
 
 interface ExerciseProps {
   weekday: Weekday;
-};
+}
 
 const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
   // Fetch all exercises for the given weekday from local storage
@@ -28,38 +28,18 @@ const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
   //form, when submitted send to localstorage. Give write an exercisemap,
   // pair weekdayexercies with the weekday and send to localstorageWrite
 
-  const [exercises, setExercises] = useState(weekdayExercises);
-  
+  const [exercises, setExercises] = useState<ExerciseObject[]>([]);
+  // const [exercises, setExercises] = useState(weekdayExercises);
+
   useEffect(() => {
     weekdayExercises = useLocalStorageRead(weekday);
     setExercises(weekdayExercises);
   }, [weekday]); // Dependency array includes 'weekday' to re-run the effect when it changes
-  // -------------------------------------------------------------------------------------------
 
-  useEffect(() => {
-    const newExercises = useLocalStorageRead(weekday);
-    setExercises(newExercises);
-  }, [weekday]);
+  // save new sets to localstorage
+  // when adding a new set, make the update show directly.
+  // currently only updates the page if switching day and then returning.
 
-  useEffect(() => {
-    useLocalStorageWrite(exercises);
-
-  }, [exercises, weekday]);
-
-  useEffect(() => {
-    // Use the key 'exercises-{weekday}' to store exercises data for different weekdays separately
-    useLocalStorageWrite(`exercises-${weekday}`, exercises);
-  }, [exercises, weekday]);
-
-
-
-// save new sets to localstorage
-    // when adding a new set, make the update show directly.
-    // currently only updates the page if switching day and then returning.
-
-  
-
-  // Function to add a set to an exercise
   // CHANGE: Adds new set to selected exercise, and updates the state variable with the updated array
   const addSet = (exerciseIndex: number, weekday: Weekday): void => {
     const exercisesCopy = [...exercises];
@@ -72,7 +52,6 @@ const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
     setExercises(exercisesCopy);
   };
 
-  
   // Function to remove a set from an exercise
   // CHANGE: Removes set from selected exercise, and updates the state variable with the new amount of sets
   const removeSet = (exerciseIndex: number, setIndex: number): void => {
@@ -129,20 +108,21 @@ const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
     );
   };
 
-  // Function to toggle an exercise as completed (assuming you have a completed flag in ExerciseObject)
-  // const toggleExercise = (exerciseIndex: number) => {
-  //   setExercises((currentExercises) => 
-  //     currentExercises.map((exercise, index) => {
-  //       if (index === exerciseIndex) {
-  //         // Toggle the visibility
-  //         return { ...exercise, isVisible: !exercise.isVisible };
-  //       }
-  //       return exercise;
-  //     })
-  //   );
-  // };
-  
-    
+  const [hiddenExercises, setHiddenExercises] = useState(new Set<number>());
+  //toggles whether the sets of an exercise are hidden.
+  //It works with a Set that keeps track of the indexes of exercises whose details are currently hidden.
+  const toggleExerciseVisibility = (exerciseIndex: number) => {
+    setHiddenExercises((prevHiddenExercises) => {
+      const newHiddenExercises = new Set(prevHiddenExercises);
+      if (newHiddenExercises.has(exerciseIndex)) {
+        newHiddenExercises.delete(exerciseIndex);
+      } else {
+        newHiddenExercises.add(exerciseIndex);
+      }
+      return newHiddenExercises;
+    });
+  };
+
   const updateSetDetails = (
     exerciseIndex: number,
     setIndex: number,
@@ -156,7 +136,7 @@ const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
       // Since field is now guaranteed to be a key of WorkingSet, TypeScript knows this indexing is safe
       const setToUpdate: WorkingSet = { ...exerciseToUpdate.sets[setIndex] };
 
-      // Make sure to handle conversion safely. Since 'completed' is a boolean, it shouldn't be converted to number
+      // Make sure to handle conversion safely. Since "completed" is a boolean, it shouldn't be converted to number
       if (field === "repetitions" || field === "weight") {
         setToUpdate[field] = Number(value);
       } else if (field === "completed") {
@@ -175,87 +155,80 @@ const Exercise: React.FC<ExerciseProps> = ({ weekday }) => {
       {exercises.map((exercise, exerciseIndex) => (
         <div key={exerciseIndex} style={styles.exerciseContainer}>
           <div style={styles.exerciseHeader}>
-            <h3 style={exercise.completed ? styles.h3Hover : styles.h3}>
-              {exercise.name} {" "}
-            </h3>
-              <strong>Total Sets: {exercise.sets.length}</strong>
-            <div>
-            <button
-                // onClick={() => toggleExercise(exerciseIndex)}
-                // style={styles.exerciseButton}
-              >
-                🔽
-              </button>
-              
-              <label style={styles.flexLabel}>
-                All sets completed
-                <input
-                  type="checkbox"
-                  checked={exercise.sets.every((set) => set.completed)}
-                  onChange={() => toggleAllSetsCompleted(exerciseIndex)}
-                />{" "}
-              </label>
-            </div>
+            <h3 style={styles.h3}>{exercise.name}</h3>
+            <strong>Total Sets: {exercise.sets.length}</strong>
+            <button onClick={() => toggleExerciseVisibility(exerciseIndex)}>
+              <img
+                src="/wireframes/expandArrow.png"
+                style={styles.visibilityButtonImg}
+                alt="Toggle visibilityButton"
+              />
+            </button>
+            <label style={styles.flexLabel}>
+              Toggle sets
+              <input
+                type="checkbox"
+                checked={exercise.sets.every((set) => set.completed)}
+                onChange={() => toggleAllSetsCompleted(exerciseIndex)}
+              />
+            </label>
           </div>
-          {/* {exercise.isVisible && exercise.sets.map((set, setIndex) => (
-    ))} */}
-          {exercise.sets.map((set, setIndex) => (
-            <div key={setIndex} style={styles.inputAndButtonContainer}>
+          {!hiddenExercises.has(exerciseIndex) && (
+            <div>
+              {exercise.sets.map((set, setIndex) => (
+                <div key={setIndex} style={styles.inputAndButtonContainer}>
+                  <button onClick={() => removeSet(exerciseIndex, setIndex)}>
+                    ❌
+                  </button>
+                  <span style={styles.labelSpan}>Set {setIndex + 1}:</span>
+                  Reps
+                  <input
+                    type="number"
+                    style={styles.setInput}
+                    value={set.repetitions}
+                    onChange={(e) =>
+                      updateSetDetails(
+                        exerciseIndex,
+                        setIndex,
+                        "repetitions",
+                        e.target.value
+                      )
+                    }
+                  />{" "}
+                  Weight
+                  <input
+                    type="number"
+                    style={styles.setInput}
+                    value={set.weight}
+                    onChange={(e) =>
+                      updateSetDetails(
+                        exerciseIndex,
+                        setIndex,
+                        "weight",
+                        e.target.value
+                      )
+                    }
+                  />{" "}
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={set.completed}
+                      onChange={() =>
+                        toggleSetCompleted(exerciseIndex, setIndex)
+                      }
+                    />
+                  </label>
+                  {/* Completed */}
+                </div>
+              ))}
               <button
-                onClick={() => removeSet(exerciseIndex, setIndex)}
-                // style={styles.removeButton}
+                onClick={() => addSet(exerciseIndex, weekday)}
+                style={styles.addButton}
               >
-                ❌
-              </button>             
-              <span>Set {setIndex + 1}:</span>
-              <input
-                type="number"
-                style={styles.setInput}
-                value={set.repetitions}
-                onChange={(e) =>
-                  updateSetDetails(
-                    exerciseIndex,
-                    setIndex,
-                    "repetitions",
-                    e.target.value
-                  )
-                }
-              />{" "}
-              Reps
-              <input
-                type="number"
-                style={styles.setInput}
-                value={set.weight}
-                onChange={(e) =>
-                  updateSetDetails(
-                    exerciseIndex,
-                    setIndex,
-                    "weight",
-                    e.target.value
-                  )
-                }
-              />{" "}
-              Weight
-              <label>
-                <input
-                  type="checkbox"
-                  checked={set.completed}
-                  onChange={() => toggleSetCompleted(exerciseIndex, setIndex)}
-                />
-              </label>
+                Add Set
+              </button>
             </div>
-          ))}
-           <button
-
-onClick={() => addSet(exerciseIndex, weekday)}
-
-style={styles.addButton}
-
->
-
-Add Set
-
-</button>
+          )}
         </div>
       ))}
     </div>
@@ -264,12 +237,11 @@ Add Set
 
 const styles = {
   exerciseContainer: {
-    marginTop: "10px",
+    backgroundColor: "#f9f9f9",
+    border: "1px solid #e1e1e1",
+    borderRadius: "11px",
+    padding: "19px",
     marginBottom: "20px",
-    border: "1px solid #ccc",
-    borderRadius: "8px",
-    padding: "10px",
-    backgroundColor: "#FFFAFA",
   },
   flexLabel: {
     display: "flex",
@@ -281,17 +253,18 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: "15px",
+    marginBottom: "20px",
   },
   setContainer: {
     display: "flex",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: "5px",
+    marginBottom: "50px",
   },
   setInput: {
-    margin: "0 5px",
-    padding: "5px",
+    margin: " 18px",
+    padding: "11px",
     width: "60px",
     borderRadius: "4px",
     border: "1px solid #ccc",
@@ -301,7 +274,8 @@ const styles = {
     display: "flex",
     alignItems: "center",
     justifyContent: "space-between",
-    flex: 1,
+    marginBottom: "11px",
+    // flex: 1,
   },
 
   inputStyle: {
@@ -311,36 +285,21 @@ const styles = {
     borderRadius: "4px",
   },
 
-  // exerciseButton: {
-  //   background: "#008CBA",
-  //   color: "white",
-  //   border: "none",
-  //   borderRadius: "4px",
-  //   padding: "5px 10px",
-  //   cursor: "pointer",
-  //   margin: "0 10px",
-  //   marginRight: "30px",
-  // },
+  visibilityButtonImg: {
+    width: "20px",
+    height: "20px",
+  },
 
   addButton: {
     display: "inline-block",
     backgroundColor: "#4CAF50",
+    // border: "none",
     color: "white",
-    padding: "8px 12px",
-    borderRadius: "4px",
+    padding: "10px 15px",
+    borderRadius: "6px",
     cursor: "pointer",
     marginTop: "10px",
   },
-
-  // removeButton: {
-  //   cursor: 'pointer',
-  //   marginLeft: '10px',
-  //   backgroundColor: '#f44336',
-  //   color: 'white',
-  //   border: 'none',
-  //   borderRadius: '4px',
-  //   padding: '5px 10px',
-  // },
 
   totalSetsInfo: {
     fontWeight: "bold",
@@ -348,11 +307,17 @@ const styles = {
   },
   h3: {
     cursor: "pointer",
+    margin: "0",
+    fontSize: "20px",
   },
   h3Hover: {
     backgroundColor: "#f2f2f2",
     color: "#007bff",
     textDecoration: "underline",
+  },
+
+  labelSpan: {
+    marginRight: "20px", // Adjust as needed
   },
 };
 

9d40827b44d3cffa5b30fbc9010766393fc17289
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-03-14 11:46:49 +0100

    feat: move jsx and functions to components

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import type { ExerciseObject, Unit, Weekday, WorkingSet } from "../types";
+import type { ExerciseObject, Unit, Weekday } from "../types";
 import { useLocalStorageRead } from "../hooks/useLocalStorageRead";
 import { useLocalStorageOverwrite } from "../hooks/useLocalStorageOverwrite";
 import "../stylesheets/Exercise.css";
+import WorkingSet from "./WorkingSet";
+import TogglePageVisibilityButton from "./ToggleExerciseVisibilityButton";
+import RemoveExerciseButton from "./RemoveExerciseButton";
 
 // add function to click on exercise in order to show, and hide sets
 // Exercise completed should close the exercise when checked.
@@ -20,11 +23,7 @@ interface ExerciseProps {
   getExerciseData: () => void;
 }
 
-const Exercise: React.FC<ExerciseProps> = ({
-  weekday,
-  exerciseData: weekExerciseListLength,
-  getExerciseData,
-}) => {
+const Exercise: React.FC<ExerciseProps> = ({ weekday, exerciseData: weekExerciseListLength, getExerciseData }) => {
   // Fetch all exercises for the given weekday from local storage
   let weekdayExercises: ExerciseObject[] = [];
 
@@ -56,54 +55,12 @@ const Exercise: React.FC<ExerciseProps> = ({
 
     // clears localStorage of selected day and writes new exercises to it
     // runs getExerciseData to make sure every other component based on data in localStorage re-renders
-    useLocalStorageOverwrite(
-      new Map<Weekday, ExerciseObject[]>([[weekday, exercises]])
-    );
+    useLocalStorageOverwrite(new Map<Weekday, ExerciseObject[]>([[weekday, exercises]]));
     getExerciseData();
   };
 
-  // Function to remove a set from an exercise
-  // CHANGE: Removes set from selected exercise, and updates the state variable with the new amount of sets
-  const removeSet = (exerciseIndex: number, setIndex: number): void => {
-    const exercisesCopy = [...exercises];
-    const selectedExercise = exercisesCopy[exerciseIndex];
+  const [hiddenExercises, setHiddenExercises] = useState<Set<number>>(new Set<number>());
 
-    // Use splice to remove the selected set
-    selectedExercise.sets.splice(setIndex, 1);
-
-    // Replace the old exercise with the updated one
-    exercisesCopy.splice(exerciseIndex, 1, selectedExercise);
-
-    setExercises(exercisesCopy);
-    // Here, you should also update local storage to reflect the changes
-    // This can be done by calling your useLocalStorageWrite hook or similar functionality
-
-    // clears localStorage of selected day and writes new exercises to it
-    // runs getExerciseData to make sure every other component based on data in localStorage re-renders
-    useLocalStorageOverwrite(
-      new Map<Weekday, ExerciseObject[]>([[weekday, exercises]])
-    );
-    getExerciseData();
-  };
-
-  // Function to toggle a set as completed
-  const toggleSetCompleted = (exerciseIndex: number, setIndex: number) => {
-    const exercisesCopy = [...exercises];
-    const selectedExercise = exercisesCopy[exerciseIndex];
-    const selectedSet = selectedExercise.sets[setIndex];
-
-    selectedSet.completed = !selectedSet.completed;
-
-    exercisesCopy[exerciseIndex].sets.splice(setIndex, 1, selectedSet);
-    setExercises(exercisesCopy);
-
-    // clears localStorage of selected day and writes new exercises to it
-    // runs getExerciseData to make sure every other component based on data in localStorage re-renders
-    useLocalStorageOverwrite(
-      new Map<Weekday, ExerciseObject[]>([[weekday, exercises]])
-    );
-    getExerciseData();
-  };
 
   // Function to toggle all sets as completed for an exercise
   const toggleAllSetsCompleted = (exerciseIndex: number) => {
@@ -133,90 +90,21 @@ const Exercise: React.FC<ExerciseProps> = ({
 
     // clears localStorage of selected day and writes new exercises to it
     // runs getExerciseData to make sure every other component based on data in localStorage re-renders
-    useLocalStorageOverwrite(
-      new Map<Weekday, ExerciseObject[]>([[weekday, exercises]])
-    );
+    useLocalStorageOverwrite(new Map<Weekday, ExerciseObject[]>([[weekday, exercises]]));
     getExerciseData();
   };
 
-  const [hiddenExercises, setHiddenExercises] = useState(new Set<number>());
-  //toggles whether the sets of an exercise are hidden.
-  //It works with a Set that keeps track of the indexes of exercises whose details are currently hidden.
-  const toggleExerciseVisibility = (exerciseIndex: number) => {
-    setHiddenExercises((prevHiddenExercises) => {
-      const newHiddenExercises = new Set(prevHiddenExercises);
-      if (newHiddenExercises.has(exerciseIndex)) {
-        newHiddenExercises.delete(exerciseIndex);
-      } else {
-        newHiddenExercises.add(exerciseIndex);
-      }
-      return newHiddenExercises;
-    });
-  };
-
-  const updateSetDetails = (
-    exerciseIndex: number,
-    setIndex: number,
-    field: keyof WorkingSet, // Use keyof to ensure field is a valid key
-    value: string
-  ) => {
-    setExercises((currentExercises) => {
-      const updatedExercises = [...currentExercises];
-      const exerciseToUpdate = { ...updatedExercises[exerciseIndex] };
-
-      // Since field is now guaranteed to be a key of WorkingSet, TypeScript knows this indexing is safe
-      const setToUpdate: WorkingSet = { ...exerciseToUpdate.sets[setIndex] };
-
-      // Make sure to handle conversion safely. Since "completed" is a boolean, it shouldn't be converted to number
-      if (field === "repetitions" || field === "weight") {
-        setToUpdate[field] = Number(value);
-      } else if (field === "completed") {
-        setToUpdate[field] = value === "true";
-      }
-
-      exerciseToUpdate.sets[setIndex] = setToUpdate;
-      updatedExercises[exerciseIndex] = exerciseToUpdate;
-
-      // clears localStorage of selected day and writes new exercises to it
-      // runs getExerciseData to make sure every other component based on data in localStorage re-renders
-      useLocalStorageOverwrite(
-        new Map<Weekday, ExerciseObject[]>([[weekday, exercises]])
-      );
-      getExerciseData();
-
-      return updatedExercises;
-    });
-  };
-
-  // removes the selected exercise from current weekday
-  function removeExercise(exerciseIndex: number): void {
-    const updatedExercises = [...exercises];
-
-    // Use splice to remove the selected exercise
-    updatedExercises.splice(exerciseIndex, 1);
-
-    // Update local storage with the new set of exercises
-    useLocalStorageOverwrite(
-      new Map<Weekday, ExerciseObject[]>([[weekday, updatedExercises]])
-    );
-    getExerciseData();
-  }
-
+  // set component
+  // buttons to components?
   return (
     <div className="Exercise">
       {exercises.map((exercise, exerciseIndex) => (
-        <div key={exerciseIndex} className="exerciseContainer" >
-            <button onClick={() => toggleExerciseVisibility(exerciseIndex)}>
-              <img
-                src="/src/assets/rightFacingArrow.svg"
-                className="visibilityButtonImg"
-                alt="Toggle visibilityButton"
-              />
-            </button>
+        <div key={exerciseIndex} className="exerciseContainer">
+          <TogglePageVisibilityButton exerciseIndex={exerciseIndex} setHiddenExercises={setHiddenExercises}/>
           <div className="exerciseHeader">
             <h3>{exercise.name}</h3>
             <strong>Total Sets: {exercise.sets.length}</strong>
-          
+
             <label className="flexLabel">
               Toggle sets
               <input
@@ -225,68 +113,22 @@ const Exercise: React.FC<ExerciseProps> = ({
                 onChange={() => toggleAllSetsCompleted(exerciseIndex)}
               />
             </label>
-            <label>
-              <input
-                type="button"
-                value="del"
-                onClick={() => removeExercise(exerciseIndex)}
-              ></input>
-            </label>
+            <RemoveExerciseButton exerciseIndex={exerciseIndex} weekday={weekday} exercises={exercises} getExerciseData={getExerciseData}/>
           </div>
           {!hiddenExercises.has(exerciseIndex) && (
-            <div>
-              {exercise.sets.map((set, setIndex) => (
-                <div key={setIndex} className="inputAndButtonContainer">
-                  <button onClick={() => removeSet(exerciseIndex, setIndex)}>
-                    ❌
-                  </button>
-                  <span className="labelSpan">Set {setIndex + 1}:</span>
-                  Reps
-                  <input
-                    type="number"
-                    className="setInput"
-                    value={set.repetitions}
-                    onChange={(e) =>
-                      updateSetDetails(
-                        exerciseIndex,
-                        setIndex,
-                        "repetitions",
-                        e.target.value
-                      )
-                    }
-                  />
-                  Weight
-                  <input
-                    type="number"
-                    className="setInput"
-                    value={set.weight}
-                    onChange={(e) =>
-                      updateSetDetails(
-                        exerciseIndex,
-                        setIndex,
-                        "weight",
-                        e.target.value
-                      )
-                    }
-                  />
-                  <label>
-                    <input
-                      type="checkbox"
-                      checked={set.completed}
-                      onChange={() => {
-                        toggleSetCompleted(exerciseIndex, setIndex);
-                      }}
-                    />
-                  </label>
-                </div>
-              ))}
-              <button
-                onClick={() => addSet(exerciseIndex, weekday)}
-                className="addButton"
-              >
+            <>
+              <WorkingSet
+                weekday={weekday}
+                exercise={exercise}
+                exerciseIndex={exerciseIndex}
+                setExercises={setExercises}
+                getExerciseData={getExerciseData}
+                exercises={exercises}
+              />
+              <button onClick={() => addSet(exerciseIndex, weekday)} className="addButton">
                 Add Set
               </button>
-            </div>
+            </>
           )}
         </div>
       ))}
---
+++ src/components/ToggleExerciseVisibilityButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface TogglePageVisibilityButtonProps {
+  exerciseIndex: number;
+  setHiddenExercises: React.Dispatch<React.SetStateAction<Set<number>>>;
+}
+
+const TogglePageVisibilityButton: React.FC<TogglePageVisibilityButtonProps> = ({
+  exerciseIndex,
+  setHiddenExercises,
+}) => {
+  //toggles whether the sets of an exercise are hidden.
+  //It works with a Set that keeps track of the indexes of exercises whose details are currently hidden.
+  const toggleExerciseVisibility = (exerciseIndex: number) => {
+    setHiddenExercises((prevHiddenExercises) => {
+      const newHiddenExercises = new Set(prevHiddenExercises);
+      if (newHiddenExercises.has(exerciseIndex)) {
+        newHiddenExercises.delete(exerciseIndex);
+      } else {
+        newHiddenExercises.add(exerciseIndex);
+      }
+      return newHiddenExercises;
+    });
+  };
+
+  return (
+    <button onClick={() => toggleExerciseVisibility(exerciseIndex)}>
+      <img src="/src/assets/rightFacingArrow.svg" className="visibilityButtonImg" alt="Toggle visibilityButton" />
+    </button>
+  );
+};
+
+export default TogglePageVisibilityButton;
